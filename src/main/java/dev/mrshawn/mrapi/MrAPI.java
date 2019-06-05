5de02d3c869fdc25a9d39bf4b0d47cb1452652c5
MrShawn <thunderboy288@gmail.com>
2019-06-05 19:57:16 -0400

    Added startup message. This is just for testing version numbers.

--- src/main/java/dev/mrshawn/mrapi/MrAPI.java
+++ src/main/java/dev/mrshawn/mrapi/MrAPI.java
@@ -1,12 +1,13 @@
 package dev.mrshawn.mrapi;
 
+import dev.mrshawn.mrapi.utils.Common;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MrAPI extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
+		Common.log("MrAPI", "&6MrAPI started successfully!");
 
 	}
 
